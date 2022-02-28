d576bca8ccc786bf9aa58f742f1d02c88e3e62ba
Kevin Pantoja <kevinpantoja146@gmail.com>
2022-02-28 09:45:27 -0500

    iniciando el proyecto

---
+++ js/index.js
@@ -0,0 +1,40 @@
+//evento de estado en footer responsive
+let estado_footer = document.getElementsByClassName("footer__estado");
+for(let estado of estado_footer){
+    estado.onclick = (e)=>{
+        let icono = estado.children[0];
+        let contenido = icono.parentNode.parentNode.parentNode.children[1];
+        if(icono.classList.contains("fa-angle-down")){
+            icono.classList.remove("fa-angle-down");
+            icono.classList.add("fa-angle-up");
+            contenido.style.display="block";
+        }else{
+            icono.classList.remove("fa-angle-up");
+            icono.classList.add("fa-angle-down");
+            contenido.style.display="none";
+        }
+    }
+}
+const footer__estado__control = (e)=>{
+    if(window.innerWidth > 500){
+        let estado_footer = document.getElementsByClassName("footer__estado");
+        for(let estado of estado_footer){
+                let icono = estado.children[0];
+                let contenido = icono.parentNode.parentNode.parentNode.children[1];
+                contenido.style.display="block";
+        }
+    }
+    else{
+        let estado_footer = document.getElementsByClassName("footer__estado");
+        for(let estado of estado_footer){
+            let icono = estado.children[0];
+            let contenido = icono.parentNode.parentNode.parentNode.children[1];
+            if(icono.classList.contains("fa-angle-down")){
+                contenido.style.display="none";
+            }     
+        }
+
+    }
+}
+window.addEventListener("resize",footer__estado__control);
+window.addEventListener("load",footer__estado__control);
